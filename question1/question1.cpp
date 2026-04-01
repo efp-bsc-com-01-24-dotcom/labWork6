35744e31edc27f9a62d59e05b95d34bb1b6bdc1a
efp-bsc-com-01-24-dotcom <efp-bsc-com-01-24@unima.ac.mw>
2026-04-01 15:01:13 +0200

    modifued question.cpp

--- question1/question1.cpp
+++ question1/question1.cpp
@@ -3,6 +3,35 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int numberOfobjects;
+    string objectStr;
+    int objectInt;
+    cout<<"Enter the number of objects: ";
+    cin>>numberOfobjects;
+
+    int* dynamicArrayint = nullptr;
+    string* dynamicArraystr = nullptr;
+
+    dynamicArrayint = new int[numberOfobjects];
+    dynamicArraystr = new string[numberOfobjects];
+
+    if(dynamicArrayint == nullptr || dynamicArraystr == nullptr) {
+        cout<<"Memory allocation failed!"<<endl;
+        return 1;
+    }else{
+        for(int i=0; i<numberOfobjects; i++) {
+            cout<<"Enter object "<<i+1<<" (string and integer): ";
+            cin>>objectStr>>objectInt;
+            dynamicArraystr[i] = objectStr;
+            dynamicArrayint[i] = objectInt;
+        }
+        for(int i=0; i<numberOfobjects; i++) {
+            cout<<"you enters: "<<dynamicArraystr[i]<<" & "<<dynamicArrayint[i]<<endl;
+        }
+        delete [] dynamicArrayint;
+        delete [] dynamicArraystr;
+    }
+
+
     return 0;
 }
