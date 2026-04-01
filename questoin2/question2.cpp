95210187f175435909bc82e23269295ee84b6644
efp-bsc-com-01-24-dotcom <efp-bsc-com-01-24@unima.ac.mw>
2026-04-01 15:25:47 +0200

    modified indentation in question6.cpp

--- questoin2/question2.cpp
+++ questoin2/question2.cpp
@@ -8,39 +8,37 @@ int main(){
     cin>>dimetion1>>dimetion2;  
     int** dynamicArray2D = nullptr;
     dynamicArray2D = new int*[dimetion1];
-    if(dynamicArray2D == nullptr) {
-        cout<<"Memory allocation failed!"<<endl;
-        return 1;
-    }else{
-        if(dimetion2>3||dimetion1>3){
-            cout<<"Dimetions should not be greater than 3!"<<endl;
+        if(dynamicArray2D == nullptr) {
+            cout<<"Memory allocation failed!"<<endl;
             return 1;
-        }else{      
-        for(int i=0; i<dimetion1; i++) {
-            dynamicArray2D[i] = new int[dimetion2];
-            if(dynamicArray2D[i] == nullptr) {
-                cout<<"Memory allocation failed!"<<endl;
+        }else{
+            if(dimetion2>3||dimetion1>3){
+                cout<<"Dimetions should not be greater than 3!"<<endl;
                 return 1;
+            }else{      
+            for(int i=0; i<dimetion1; i++) {
+                dynamicArray2D[i] = new int[dimetion2];
+                if(dynamicArray2D[i] == nullptr) {
+                    cout<<"Memory allocation failed!"<<endl;
+                    return 1;
+                }
+            }
+            for(int i=0; i<dimetion1; i++) {
+                for(int j=0; j<dimetion2; j++) {
+                    cout<<"Enter element ["<<i+1<<"]["<<j+1<<"]: ";
+                    cin>>dynamicArray2D[i][j];
+                }
+            }
+            for(int i=0; i<dimetion1; i++) {
+                for(int j=0; j<dimetion2; j++) {
+                    cout<<dynamicArray2D[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+            for(int i=0; i<dimetion1; i++) {
+                delete [] dynamicArray2D[i];
             }
         }
-        for(int i=0; i<dimetion1; i++) {
-            for(int j=0; j<dimetion2; j++) {
-                cout<<"Enter element ["<<i+1<<"]["<<j+1<<"]: ";
-                cin>>dynamicArray2D[i][j];
-            }
-        }
-        for(int i=0; i<dimetion1; i++) {
-            for(int j=0; j<dimetion2; j++) {
-                cout<<dynamicArray2D[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        for(int i=0; i<dimetion1; i++) {
-            delete [] dynamicArray2D[i];
-        }
-    }
-        delete [] dynamicArray2D;
     }
     return 0;
-
 }
